432a704574611728ca28813945710381d260353d
roshk99 <roshk99@users.noreply.github.com>
2019-11-25 16:29:02 -0500

    Added NEDI

--- image_upsampling.py
+++ image_upsampling.py
@@ -10,6 +10,7 @@ def bilinear(im, win_size=20, scale=None, height=None, width=None):
     
     if height and width:
         h_s, w_s = h/height, w/width
+        h_s = np.round(h_s, decimals=2)
     elif scale:
         h_s, w_s = 1/scale, 1/scale
         height, width = h/h_s, w/w_s
@@ -39,10 +40,7 @@ def bilinear(im, win_size=20, scale=None, height=None, width=None):
         im2.append(vert)
     im2 = np.stack(im2, axis=2)
     
-    if np.max(im) > 2:
-        im2 = np.clip(im2, 0, 255)
-    else:
-        im2 = np.clip(im2, 0, 1)
+    im2 = np.clip(im2, 0, 1)
     
     return im2
 
@@ -81,65 +79,168 @@ def bicubic(im, win_size=20, scale=None, height=None, width=None):
         im2.append(vert)
     im2 = np.stack(im2, axis=2)
     
-    if np.max(im) > 2:
-        im2 = np.clip(im2, 0, 255)
-    else:
-        im2 = np.clip(im2, 0, 1)
+    im2 = np.clip(im2, 0, 1)
     
     return im2
 
-def plot_images(original_im, ground_truth_im, upsampled_im,title=None ):
+def nedi(im, win_size=20, scale=None, height=None, width=None):
+    
+    h, w, chan = im.shape
+    
+    if int(height/h) == 4:
+        flag = True
+    else:
+#        print('Ratio', height/h)
+        flag = False
+        
+    scale = 2
+    height, width = h*2, w*2
+    im2s = []
+    
+    for kk in np.arange(chan):
+        im1 = im[:,:,kk]
+        #creating x and y matrices
+        xt = []
+        yt = []
+        for ii in range(1, h-1):
+            for jj in range(1, w-1):
+                yt.append(im1[ii][jj])
+                if (ii+jj)%2==0 :
+                    xt.append([im1[ii-1][jj-1],im1[ii-1][jj+1],\
+                               im1[ii+1][jj-1],im1[ii+1][jj+1]])
+                else :
+                    xt.append([im1[ii][jj-1],im1[ii][jj+1],\
+                               im1[ii+1][jj],im1[ii-1][jj]])
+    
+        x = np.array(xt)
+        y = np.array(yt)
+        
+        b = np.linalg.inv(x.T @ x) @ x.T @ y
+        
+        # initializing image to be predicted
+        im2 = np.zeros((height, width))
+        for ii in range(h):
+            for jj in range(w):
+                im2[scale*ii][scale*jj] = im1[ii][jj]
+        
+        for ii in range(1,height-2,scale):
+            for jj in range(1,width-2,scale):
+                im2[ii][jj] = np.matmul([im2[ii-1][jj-1],\
+                                          im2[ii-1][jj+1],\
+                                          im2[ii+1][jj-1],\
+                                          im2[ii+1][jj+1]],b)
+        
+        for ii in range(1,height-1,scale):
+            im2[ii][-1] = (im2[ii-1][-2] + im2[ii+1][-2])/2
+                     
+        for jj in range(1,width-1,2):
+            im2[-1][jj] = (im2[-2][jj-1] + im2[-2][jj+1])/2
+            
+        for ii in range(1,height-2,scale):
+            for jj in range(2,width-1,scale):
+                im2[ii][jj] = np.matmul([im2[ii][jj-1],\
+                                       im2[ii][jj+1],\
+                                       im2[ii-1][jj],\
+                                       im2[ii+1][jj]],b)
+        
+        for ii in range(2,height-1,scale):
+            for jj in range(1,width-2,scale):
+                im2[ii][jj] = np.matmul([im2[ii][jj-1],\
+                                           im2[ii][jj+1],\
+                                           im2[ii-1][jj],\
+                                           im2[ii+1][jj]],b)
+        for ii in range(2,height-1,scale):
+            im2[ii][-1] = (im2[ii-1][-2] + im2[ii+1][-2])/2
+        
+        for jj in range(1,width-1,scale):
+            im2[0][jj] = (im2[1][jj-1] + im2[1][jj+1])/2
+        
+        for ii in range(1,height-2,scale):
+            im2[ii][0] = (im2[ii-1][1] + im2[ii+1][1])/2
+            
+        for jj in range(2,width-1,scale):
+            im2[-1][jj] = (im2[-2][jj-1] + im2[-2][jj+1])/2
+            
+        im2[0][-1] = im2[1][-2]
+        im2[-1][0] = im2[-2][1]
+        
+        im2s.append(im2)
+    
+    im2 = np.stack(im2s, axis=2)
+    im2 = np.clip(im2, 0, 1)
+    
+    if flag:
+        im2 = nedi(im2, height=height, width=width)
+    
+    return im2
+    
+
+def plot_images(original_im, ground_truth_im, upsampled_im,title=None):
 
     fig, ax = plt.subplots(1, 3, sharey=True, sharex=True)
-    mng = plt.get_current_fig_manager()
-    mng.window.showMaximized()
+#    mng = plt.get_current_fig_manager()
+#    mng.window.showMaximized()
 
     if(title is not None):
         fig.suptitle("{}".format(title),fontsize=24)
 
     ax[0].imshow(original_im)
     ax[0].axis('off')
-    ax[0].set_title("Original Image: {} x {}".format(original_im.shape[0], original_im.shape[1]), fontsize=12)
+    ax[0].set_title("Original Image: {} x {}".format(original_im.shape[0], \
+          original_im.shape[1]), fontsize=12)
     ax[1].imshow(ground_truth_im)
     ax[1].axis('off')
-    ax[1].set_title("Ground Truth Image: {} x {}".format(ground_truth_im.shape[0], ground_truth_im.shape[1]), fontsize=12)
+    ax[1].set_title("Ground Truth Image: {} x {}".format(\
+          ground_truth_im.shape[0], ground_truth_im.shape[1]), fontsize=12)
     ax[2].imshow(upsampled_im)
     ax[2].axis('off')
-    ax[2].set_title("Upsampled Image: {} x {}".format(upsampled_im.shape[0], upsampled_im.shape[1]), fontsize=12)
+    ax[2].set_title("Upsampled Image: {} x {}".format(upsampled_im.shape[0], \
+          upsampled_im.shape[1]), fontsize=12)
     plt.show()
 
 def compare_images(original_im_name, ground_truth_im_name, interp_func):
     
     original_im = skimage.io.imread(original_im_name)
-    original_im = skimage.color.rgba2rgb(original_im)
-
     ground_truth_im = skimage.io.imread(ground_truth_im_name)
-    ground_truth_im = skimage.color.rgba2rgb(ground_truth_im)
-    upsampled_im = interp_func(original_im, height=ground_truth_im.shape[0], width=ground_truth_im.shape[1], win_size=20)
     
-    assert ground_truth_im.shape == upsampled_im.shape, "Interpolated Image not same size as True Image"
+    if np.max(original_im) > 2:
+        original_im = original_im / 255.0
+    if np.max(ground_truth_im) > 2:
+        ground_truth_im = ground_truth_im / 255.0
+    
+    upsampled_im = interp_func(original_im, height=ground_truth_im.shape[0], \
+                               width=ground_truth_im.shape[1], win_size=20)
+    
+    assert ground_truth_im.shape == upsampled_im.shape, \
+        "Interpolated Image not same size as True Image"
     
     mse = util.mse(ground_truth_im, upsampled_im)
     psnr = util.psnr(ground_truth_im, upsampled_im)
-    ssim, _ = skimage.measure.compare_ssim(ground_truth_im, upsampled_im, full=True, multichannel=True)
+    ssim, _ = skimage.measure.compare_ssim(ground_truth_im, upsampled_im, \
+                                           full=True, multichannel=True)
 
-    print("Resizing {} x {} to {} x {} using {} interpolation.".format(original_im.shape[0], \
-          original_im.shape[1], upsampled_im.shape[0], upsampled_im.shape[1], interp_func.__name__))
+    print("Resizing {} x {} to {} x {} using {} interpolation.".format(\
+          original_im.shape[0], \
+          original_im.shape[1], upsampled_im.shape[0], upsampled_im.shape[1], \
+          interp_func.__name__))
     print("\nEvaluation Metrics:")
     print("Mean-Squared Error is {:.4f}".format(mse))
     print("Peak Signal-to-Noise Ratio is {:.4f}".format(psnr))
     print("Structural Similarity Index is {:.4f}".format(ssim))
     plot_images(original_im, ground_truth_im,upsampled_im,interp_func.__name__)
+    print("--------------")
+    
 
 def main():
     
-    im_name = 'data/wild_animal300.png'
-    true_im_name = 'data/wild_animal600.png'
+    im_name = 'data/tiger_300_215.png'
+    true_im_name = 'data/tiger_600_430.png'
     
     print("Want Lower MSE, and Higher PSNR and SSIM\n")
     
     compare_images(im_name, true_im_name, bilinear)
     compare_images(im_name, true_im_name, bicubic)
+    compare_images(im_name, true_im_name, nedi)
     
 if __name__ == "__main__":
     main()
